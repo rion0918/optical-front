38c7171cad1552e879fdbf563320de811354eb09
y_k <ka.20.zu.04@gmail.com>
2025-12-02 13:22:52 +0900

    feat/issue1: テンプレート画面の作成

--- src/components/organisms/LandingTemplateSection/TemplateData.ts
+++ src/components/organisms/LandingTemplateSection/TemplateData.ts
@@ -20,20 +20,32 @@ export const templateData: LandingTemplate[] = [
     name: "スタンダード",
     description: "最低限の予定管理をシンプルに実現",
     targetUser: "個人利用のライトユーザー向け",
-    options: ["Option", "Option", "Option"],
+    options: [
+      "Option",
+      "Option",
+      "Option"
+    ],
   },
   {
     id: "creator",
     name: "クリエイター",
     description: "複数人での予定共有とプロジェクト管理が可能",
     targetUser: "チーム利用向け",
-    options: ["Option", "Option", "Option"],
+    options: [
+      "Option",
+      "Option",
+      "Option"
+    ],
   },
   {
     id: "family",
     name: "ファミリー",
     description: "複数人での予定共有と管理に最適化",
     targetUser: "家族、友人グループ向け",
-    options: ["Option", "Option", "Option"],
+    options: [
+      "Option",
+      "Option",
+      "Option"
+    ],
   },
 ];
